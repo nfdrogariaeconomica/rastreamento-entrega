af371764cd110bf8fdd85dd711e6016571bec7e1
nfdrogariaeconomica <nfdrogariaeconomica@gmail.com>
2026-01-16 12:46:46 -0300

    Add files via upload

---
+++ src/test/java/com/drogaria/entregas/EntregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drogaria.entregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
